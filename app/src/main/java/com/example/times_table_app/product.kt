f69aecdd0ee46dbe2b108f6be034ca603b7b395d
Dilni-DS113 <103235133+Dilni-DS113@users.noreply.github.com>
2022-09-07 11:11:29 +1000

    week 6 times table app completed (quick edit)

---
+++ app/src/main/java/com/example/times_table_app/product.kt
@@ -0,0 +1,10 @@
+package com.example.times_table_app
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class product(val oPResult: Int):Parcelable {
+}
+
--- app/src/main/java/com/example/times_table_app/productActivity.kt
+++ app/src/main/java/com/example/times_table_app/productActivity.kt
@@ -1,17 +1,30 @@
 package com.example.times_table_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import org.w3c.dom.Text
+import androidx.appcompat.app.AppCompatActivity
 
 class productActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
-        val p = intent.getIntExtra("product", 0) // this acivity revsives data from the main actvity
+        //------ Parcelbale multiple values ------
+        val p = intent.getParcelableExtra<product>("product")
         val product = findViewById<TextView>(R.id.product)
-        product.text = p.toString()
+        product.text = p?.oPResult.toString() // this is a nullable result so ?
+
+
+
+        // ------ Multiple value sending ------
+     //   val n1 = intent.getIntExtra("number1", 0) // this acivity revsives data from the main actvity
+     //   val n2 = intent.getIntExtra("number2", 0)
+     //   val product = findViewById<TextView>(R.id.product)
+     //   product.text = (n1 * n2).toString()
+
+        //------ Single value sending ------
+     //   val p = intent.getIntExtra("product", 0) // this acivity revsives data from the main actvity
+     //   val product = findViewById<TextView>(R.id.product)
+     //   product.text = p.toString()
 
 
     }
